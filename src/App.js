289e615cff4d57f4e7eae352fa732e5b621a1df4
Brunoskyy <arturbrunoferreira@gmail.com>
2021-12-13 15:17:46 -0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
-function App() {
+import React, { useEffect } from "react";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
+
+export const App = (props) => {
+  useEffect(() => {
+    console.log(props);
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div className="App">
       <header className="app-header">
         <h1>FormApp</h1>
       </header>
-      <div className="form">
-        <input type="text" />
-      </div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched, isValid }) => (
+          <Form className="form">
+            <div>
+              <label>First Name</label>
+              <Field name="firstName" data-testid="firstName" />
+              {errors.firstName && touched.firstName ? (
+                <div className="formError" data-testid="formError">
+                  {errors.firstName}
+                </div>
+              ) : null}
+            </div>
+
+            <div>
+              <label>Last Name</label>
+              <Field name="lastName" data-testid="lastName" />
+              {errors.lastName && touched.lastName ? (
+                <div className="formError" data-testid="formError">
+                  {errors.lastName}
+                </div>
+              ) : null}
+            </div>
+
+            <div>
+              <label>Email</label>
+              <Field name="email" type="email" data-testid="email" />
+              {errors.email && touched.email ? (
+                <div className="formError" data-testid="formError">
+                  {errors.email}
+                </div>
+              ) : null}
+            </div>
+            {isValid ? <div>True</div> : <div>False</div>}
+            <button type="submit" data-testid="submitButton">
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,52 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  cleanup,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Form", () => {
+  describe("Fill firstName input", () => {
+    afterEach(() => {
+      cleanup();
+    });
+    it("Should be valid if input is filled", async () => {
+      render(<App />);
+      const firstName = screen.getByTestId("firstName");
+      const lastName = screen.getByTestId("lastName");
+      const email = screen.getByTestId("email");
+      const submitFormButton = screen.getByTestId("submitButton");
+
+      fireEvent.change(firstName, { target: { value: "Artur" } });
+      fireEvent.change(lastName, { target: { value: "Bruno" } });
+      fireEvent.change(email, { target: { value: "artur@mail.com" } });
+
+      fireEvent.click(submitFormButton);
+
+      await waitFor(() => {
+        expect(screen.queryAllByTestId("formError")).toHaveLength(0);
+      });
+    });
+
+    it("Should not be valid if the inputs are empty", async () => {
+      render(<App />);
+      const firstName = screen.getByTestId("firstName");
+      const lastName = screen.getByTestId("lastName");
+      const email = screen.getByTestId("email");
+      const submitFormButton = screen.getByTestId("submitButton");
+
+      expect(firstName).toBeEmptyDOMElement();
+      expect(lastName).toBeEmptyDOMElement();
+      expect(email).toBeEmptyDOMElement();
+
+      fireEvent.click(submitFormButton);
+
+      await waitFor(() => {
+        expect(screen.getAllByTestId("formError")).toHaveLength(3);
+      });
+    });
+  });
 });
